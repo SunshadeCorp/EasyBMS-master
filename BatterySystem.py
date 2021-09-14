7f5f71524ca239e6d682ed0075cd95a7a284d7ea
Jesse Kleemann <j.kleemann@tu-braunschweig.de>
2021-09-15 00:19:33 +0200

    initial project commit, updated architecture, Added data model

---
+++ BatterySystem.py
@@ -0,0 +1,26 @@
+class BatterySystem:
+
+    modules = None
+    voltage = None
+    current = None
+    lower_voltage_limit = None
+    upper_voltage_limit = None
+    current_limit = None
+
+    def get_soc(self):
+        #todo
+
+    def get_power(self):
+        # todo
+
+    def get_highest_cell_temp(self):
+        # todo
+
+    def get_lowest_cell_temp(self):
+        # temp
+
+    def get_highest_cell_voltage(self):
+        # todo
+
+    def get_highest_voltage_cells(self):
+        # todo
