0f9749d75f29d0cccb5a83db3257caedda8d57da
Jesse Kleemann <j.kleemann@tu-braunschweig.de>
2021-09-15 21:16:28 +0200

    code formatting and limits

--- SocCurve.py
+++ SocCurve.py
@@ -1,9 +1,24 @@
 class SocCurve:
-    data_points: None
+    data_points = {
+        4.136: 1.00,
+        4.016: 0.9,
+        3.913: 0.8,
+        3.825: 0.7,
+        3.756: 0.6,
+        3.678: 0.5,
+        3.641: 0.4,
+        3.615: 0.3,
+        3.579: 0.2,
+        3.499: 0.1,
+        3.420: 0.0
+    }
 
     def __init__(self):
-        #  todo
+        # todo
+        pass
 
     def voltagte_to_soc(self, voltage):
-        # todo
+        lower_voltage = None
+        upper_voltage = None
+
         return 0.0
