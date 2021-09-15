29e607aa83880a366f4a6fb2b79095ad80c04231
Arne Mayer <arne.mayer@student.hpi.uni-potsdam.de>
2021-09-15 21:16:28 +0200

    code formatting and limits

--- BatteryManager.py
+++ BatteryManager.py
@@ -3,24 +3,29 @@ class BatteryManager:
 
     def balance(self):
         # todo
+        pass
 
     def trigger_safety_disconnect(self):
         # todo
+        pass
 
     # Event handling for critical events
 
     def on_critical_module_temperature(self):
         # todo
+        pass
 
     def on_critical_chip_temperature(self):
         # todo
+        pass
 
     def on_critical_module_voltage(self):
         # todo
+        pass
 
     def on_critical_cell_voltage(self):
         # todo
-
+        pass
 
     # Event handling for unplausible values
     # Todo: implement loud failing for development time
@@ -28,12 +33,16 @@ class BatteryManager:
 
     def on_implausible_module_temperature(self):
         # todo
+        pass
 
     def on_implausible_chip_temperature(self):
         # todo
+        pass
 
     def on_implausible_module_voltage(self):
         # todo
+        pass
 
     def on_implausible_cell_voltage(self):
         # todo
+        pass
