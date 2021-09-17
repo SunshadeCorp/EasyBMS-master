9cd4ff69dde9a0cfd25d2d71d7e912215b640536
Jesse Kleemann <j.kleemann@tu-braunschweig.de>
2021-09-17 04:29:29 +0200

    more work on limits

--- BatteryModule.py
+++ BatteryModule.py
@@ -4,12 +4,20 @@ from BatteryCell import BatteryCell
 
 
 class BatteryModule:
-    LOWER_MODULE_TEMP_LIMIT = -10  # °C
-    UPPER_MODULE_TEMP_LIMIT = 50  # °C
-    LOWER_CHIP_TEMP_LIMIT = -20  # °C
-    UPPER_CHIP_TEMP_LIMIT = 60  # °C
-    LOWER_VOLTAGE_LIMIT = 33.6  # V
-    UPPER_VOLTAGE_LIMIT = 50.4  # V
+    LOWER_MODULE_TEMP_LIMIT_CRITICAL: float = -20.0  # °C
+    UPPER_MODULE_TEMP_LIMIT_CRITICAL: float = 50.0  # °C
+    LOWER_MODULE_TEMP_LIMIT_WARNING: float = -10.0  # °C
+    UPPER_MODULE_TEMP_LIMIT_WARNING: float = 45.0  # °C
+
+    LOWER_CHIP_TEMP_LIMIT_CRITICAL: float = -40.0  # °C
+    UPPER_CHIP_TEMP_LIMIT_CRITICAL: float = 80.0  # °C
+    LOWER_CHIP_TEMP_LIMIT_WARNING: float = -30.0  # °C
+    UPPER_CHIP_TEMP_LIMIT_WARNING: float = 60.0  # °C
+
+    LOWER_VOLTAGE_LIMIT_CRITICAL: float = 33.6  # V
+    UPPER_VOLTAGE_LIMIT_CRITICAL: float = 50.4  # V
+    LOWER_VOLTAGE_LIMIT_WARNING: float = 38.4  # V
+    UPPER_VOLTAGE_LIMIT_WARNING: float = 49.8  # V
 
     def __init__(self):
         self.cells: List[BatteryCell] = None
@@ -26,6 +34,64 @@ class BatteryModule:
             soc_sum += cell.get_soc()
 
         return soc_sum / len(self.cells)
+
+    def update_measurements(self, temp1: float, temp2: float, voltage, chip_temp):
+        self.module_temp1 = temp1
+        self.module_temp2 = temp2
+        self.voltage = voltage
+        self.chip_temp = chip_temp
+
+        # todo
+        if self.has_critical_module_temp():
+            pass
+        elif self.has_warning_module_temp():
+            pass
+
+        if self.has_critical_chip_temp():
+            pass
+        elif self.has_warning_chip_temp():
+            pass
+
+        if self.has_critical_voltage():
+            pass
+        elif self.has_warning_voltage():
+            pass
+
+    def has_warning_module_temp1(self) -> bool:
+        return self.module_temp1 < self.LOWER_MODULE_TEMP_LIMIT_WARNING \
+               or self.module_temp1 > self.UPPER_MODULE_TEMP_LIMIT_WARNING
+
+    def has_warning_module_temp2(self) -> bool:
+        return self.module_temp2 < self.LOWER_MODULE_TEMP_LIMIT_WARNING \
+               or self.module_temp2 > self.UPPER_MODULE_TEMP_LIMIT_WARNING
+
+    def has_critical_module_temp1(self) -> bool:
+        return self.module_temp1 < self.LOWER_MODULE_TEMP_LIMIT_CRITICAL \
+               or self.module_temp1 > self.UPPER_MODULE_TEMP_LIMIT_CRITICAL
+
+    def has_critical_module_temp2(self) -> bool:
+        return self.module_temp2 < self.LOWER_MODULE_TEMP_LIMIT_CRITICAL \
+               or self.module_temp2 > self.UPPER_MODULE_TEMP_LIMIT_CRITICAL
+
+    def has_critical_module_temp(self) -> bool:
+        return self.has_critical_module_temp1() or self.has_critical_module_temp2()
+
+    def has_warning_module_temp(self) -> bool:
+        return self.has_warning_module_temp1() or self.has_warning_module_temp2()
+
+    def has_critical_chip_temp(self) -> bool:
+        return self.chip_temp < self.LOWER_CHIP_TEMP_LIMIT_CRITICAL \
+               or self.chip_temp > self.UPPER_CHIP_TEMP_LIMIT_CRITICAL
+
+    def has_warning_chip_temp(self) -> bool:
+        return self.chip_temp < self.LOWER_CHIP_TEMP_LIMIT_WARNING \
+               or self.chip_temp > self.UPPER_CHIP_TEMP_LIMIT_WARNING
+
+    def has_critical_voltage(self) -> bool:
+        return self.voltage < self.LOWER_VOLTAGE_LIMIT_CRITICAL or self.voltage > self.UPPER_VOLTAGE_LIMIT_CRITICAL
+
+    def has_warning_voltage(self) -> bool:
+        return self.voltage < self.LOWER_VOLTAGE_LIMIT_WARNING or self.voltage > self.UPPER_VOLTAGE_LIMIT_WARNING
 
     def trigger_missed_heartbeat_event(self):
         # todo
--- BatterySystem.py
+++ BatterySystem.py
@@ -1,16 +1,53 @@
+from typing import List
+
 from BatteryModule import BatteryModule
 
 
 class BatterySystem:
-    LOWER_VOLTAGE_LIMIT: float = 403.2  # V
-    UPPER_VOLTAGE_LIMIT: float = 604.8  # V
-    CURRENT_LIMIT: float = 30  # A
+    LOWER_VOLTAGE_LIMIT_CRITICAL: float = 403.2  # V
+    UPPER_VOLTAGE_LIMIT_CRITICAL: float = 604.8  # V
+    LOWER_VOLTAGE_LIMIT_WARNING: float = 460.8  # V
+    UPPER_VOLTAGE_LIMIT_WARNING: float = 597.6  # V
 
-    def __init__(self, battery_modules: list):
+    LOWER_CURRENT_LIMIT_WARNING: float = -30  # A
+    UPPER_CURRENT_LIMIT_WARNING: float = 30  # A
+    LOWER_CURRENT_LIMIT_CRITICAL: float = -32  # A
+    UPPER_CURRENT_LIMIT_CRITICAL: float = 32  # A
+
+    def __init__(self, battery_modules: List[BatteryModule]):
         # Uninitialized values
-        self.battery_modules: list = battery_modules
         self.voltage: float = 0
         self.current: float = 0
+
+        self.is_initialized = False
+        self.battery_modules: list = battery_modules
+
+    def update_measurements(self, voltage: float, current: float):
+        self.voltage = voltage
+        self.current = current
+        self.is_initialized = True
+
+        if self.has_critical_voltage():
+            pass
+        elif self.has_warning_voltage(self):
+            pass
+
+        if self.has_critical_current():
+            pass
+        elif self.has_warning_current(self):
+            pass
+
+    def has_critical_voltage(self) -> bool:
+        return self.voltage < self.LOWER_VOLTAGE_LIMIT_CRITICAL or self.voltage > self.UPPER_VOLTAGE_LIMIT_CRITICAL
+
+    def has_warning_voltage(self) -> bool:
+        return self.voltage < self.LOWER_VOLTAGE_LIMIT_WARNING or self.voltage > self.UPPER_VOLTAGE_LIMIT_WARNING
+
+    def has_critical_current(self) -> bool:
+        return self.voltage < self.LOWER_CURRENT_LIMIT_CRITICAL or self.voltage > self.UPPER_CURRENT_LIMIT_CRITICAL
+
+    def has_warning_current(self) -> bool:
+        return self.voltage < self.LOWER_CURRENT_LIMIT_WARNING or self.voltage > self.UPPER_CURRENT_LIMIT_WARNING
 
     def get_soc(self) -> float:
         soc_sum: float = 0
