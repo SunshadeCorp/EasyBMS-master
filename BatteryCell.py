cbfe5183774b04d09bc5fc4176176714a7688e54
Arne Mayer <arne.mayer@student.hpi.uni-potsdam.de>
2021-09-17 04:29:29 +0200

    more work on limits

--- BatteryCell.py
+++ BatteryCell.py
@@ -3,8 +3,10 @@ from SocCurve import SocCurve
 
 
 class BatteryCell:
-    LOWER_VOLTAGE_LIMIT: float = 2.8  # V
-    UPPER_VOLTAGE_LIMIT: float = 4.2  # V
+    LOWER_VOLTAGE_LIMIT_CRITICAL: float = 2.8  # V
+    UPPER_VOLTAGE_LIMIT_CRITICAL: float = 4.2  # V
+    LOWER_VOLTAGE_LIMIT_WARNING: float = 3.2  # V
+    UPPER_VOLTAGE_LIMIT_WARNING: float = 4.15  # V
 
     def __init__(self, soc_curve: SocCurve, battery_module: BatteryModule):
         # Uninitialized values
@@ -15,9 +17,21 @@ class BatteryCell:
         self.soc_curve: SocCurve = soc_curve
         self.parent_module: BatteryModule = battery_module
 
-    def get_soc(self):
-        # todo
-        pass
+    def get_soc(self) -> float:
+        return self.soc_curve.voltage_to_soc(self.voltage)
+
+    def has_critical_voltage(self) -> bool:
+        return self.voltage < self.LOWER_CRITICAL_VOLTAGE_LIMIT or self.voltage > self.UPPER_VOLTAGE_CRITICAL__LIMIT
+
+    def has_warning_voltage(self) -> bool:
+        return self.voltage < self.LOWER_WARNING_VOLTAGE_LIMIT or self.voltage > self.UPPER_WARNING_VOLTAGE_LIMIT
+
+    def update_voltage(self, voltage: float):
+        self.voltage = voltage
+        if self.has_critical_voltage():
+            self.trigger_critical_voltage_event()
+        elif self.has_warning_voltage():
+            self.trigger_warning_voltage_event()
 
     def start_balance_discharge(self):
         # todo
@@ -27,14 +41,17 @@ class BatteryCell:
         # todo
         pass
 
-    def is_balance_discharging(self):
-        #  todo
-        pass
+    def is_balance_discharging(self) -> bool:
+        return self.balance_pin_state
 
     def trigger_implausible_voltage_event(self):
         # todo
         pass
 
     def trigger_critical_voltage_event(self):
+        # todo
+        pass
+
+    def trigger_warning_voltage_event(self):
         # todo
         pass
