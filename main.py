2bb5a562e6f2c6d9d6694f76c1fc943fead916fd
Jesse Kleemann <j.kleemann@tu-bs.de>
2021-09-19 18:36:02 +0200

    scheduler, heartbeat

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sched
+import threading
+import time
 from typing import Any, Dict
 
 import paho.mqtt.client as mqtt
@@ -9,9 +12,14 @@ class EasyBMSMaster:
         self.mqtt_client.on_connect = self.mqtt_on_connect
         self.mqtt_client.on_message = self.mqtt_on_message
 
-        self.mqtt_client.connect(host="broker.hivemq.com", port=1883, keepalive=60)
+        self.mqtt_client.connect(host='192.168.0.191', port=1883, keepalive=60)
 
+    def loop(self):
         self.mqtt_client.loop_forever()
+
+    def send_heartbeat(self):
+        self.mqtt_client.publish(topic='master/uptime', payload=f'{time.time() * 1000:.0f}')
+        scheduler.enter(1, 1, self.send_heartbeat)
 
     def mqtt_on_connect(self, client: mqtt.Client, userdata: Any, flags: Dict, rc: int):
         pass
@@ -21,4 +29,10 @@ class EasyBMSMaster:
 
 
 if __name__ == '__main__':
-    EasyBMSMaster()
+    easy_bms_master = EasyBMSMaster()
+    mqtt_client_thread = threading.Thread(name='EasyBMSMaster', target=easy_bms_master.loop, daemon=True)
+    mqtt_client_thread.start()
+
+    scheduler = sched.scheduler()
+    scheduler.enter(1, 1, easy_bms_master.send_heartbeat)
+    scheduler.run()
