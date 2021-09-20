02fa0e230ec1e35cd355f046740ac9ccae59786d
Arne Mayer <arne.mayer@student.hpi.uni-potsdam.de>
2021-09-20 21:22:44 +0200

    more work on events

--- BatteryModule.py
+++ BatteryModule.py
@@ -1,6 +1,9 @@
+import time
 from typing import List
 
 from BatteryCell import BatteryCell
+from HeartbeatEvent import HeartbeatEvent
+from MeasurementEvent import MeasurementEvent
 
 
 class BatteryModule:
@@ -14,18 +17,38 @@ class BatteryModule:
     LOWER_CHIP_TEMP_LIMIT_WARNING: float = -30.0  # °C
     UPPER_CHIP_TEMP_LIMIT_WARNING: float = 60.0  # °C
 
-    LOWER_VOLTAGE_LIMIT_CRITICAL: float = 33.6  # V
+    LOWER_VOLTAGE_LIMIT_CRITICAL: float = 36.0  # V
     UPPER_VOLTAGE_LIMIT_CRITICAL: float = 50.4  # V
     LOWER_VOLTAGE_LIMIT_WARNING: float = 38.4  # V
     UPPER_VOLTAGE_LIMIT_WARNING: float = 49.8  # V
 
+    ESP_TIMEOUT: float = 5.000  # Seconds
+
     def __init__(self) -> None:
+        # Uninitialized
         self.cells: List[BatteryCell] = None
         self.voltage: float = None
         self.module_temp1: float = None
         self.module_temp2: float = None
         self.chip_temp: float = None
-        self.heartbeat_timestamp: int = None
+        self.last_esp_uptime: int = None
+        self.last_esp_uptime_in_own_time: float = None
+
+        # Initialized
+        self.keep_monitoring_heartbeats: bool = True
+
+        # Events
+        self.module_temp_event = MeasurementEvent()
+        self.chip_temp_event = MeasurementEvent()
+        self.heartbeat_event = HeartbeatEvent()
+
+    def heartbeat_monitor_thread(self):
+        while self.keep_monitoring_heartbeats:
+            own_time: float = time.time()
+
+            if self.last_esp_uptime_in_own_time - own_time > self.ESP_TIMEOUT:
+                self.heartbeat_event.on_heartbeat_missed()
+            time.sleep(1000)
 
     def get_soc(self) -> float:
         soc_sum: float = 0
@@ -35,11 +58,14 @@ class BatteryModule:
 
         return soc_sum / len(self.cells)
 
-    def update_measurements(self, temp1: float, temp2: float, voltage, chip_temp) -> None:
+    def update_measurements(self, temp1: float, temp2: float, voltage: float, chip_temp: float, esp_uptime: int) -> None:
         self.module_temp1 = temp1
         self.module_temp2 = temp2
         self.voltage = voltage
         self.chip_temp = chip_temp
+        self.last_esp_uptime = esp_uptime
+
+        self.heartbeat_event.on_heartbeat()
 
         # todo
         if self.has_critical_module_temp():
@@ -92,23 +118,3 @@ class BatteryModule:
 
     def has_warning_voltage(self) -> bool:
         return self.voltage < self.LOWER_VOLTAGE_LIMIT_WARNING or self.voltage > self.UPPER_VOLTAGE_LIMIT_WARNING
-
-    def trigger_missed_heartbeat_event(self) -> None:
-        # todo
-        pass
-
-    def trigger_critical_temperature_event(self) -> None:
-        # todo
-        pass
-
-    def trigger_implausible_temperature_event(self) -> None:
-        # todo
-        pass
-
-    def trigger_critical_module_voltage_event(self) -> None:
-        # todo
-        pass
-
-    def trigger_implausible_module_voltage_event(self) -> None:
-        # todo
-        pass
