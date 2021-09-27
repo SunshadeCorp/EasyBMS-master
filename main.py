cf55172ea559eda6b0577571a62fdf91c53d2421
Jesse Kleemann <j.kleemann@tu-bs.de>
2021-09-27 19:10:38 +0200

    update mqtt service

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
+import paho.mqtt.client as mqtt
 import sched
 import time
-import paho.mqtt.client as mqtt
+import yaml
+from pathlib import Path
+
 from battery_manager import BatteryManager
 from battery_system import BatterySystem
 from typing import Any, Dict
 
 
+def get_config(filename: str) -> Dict:
+    with open(Path(__file__).parent / filename, 'r') as file:
+        try:
+            cfg = yaml.safe_load(file)
+            return cfg
+        except yaml.YAMLError as e:
+            print(e)
+
+
 class EasyBMSMaster:
-    def __init__(self):
+    def __init__(self, master_config):
+        credentials = get_config('credentials.yaml')
+
         self.mqtt_client = mqtt.Client()
         self.mqtt_client.on_connect = self.mqtt_on_connect
         self.mqtt_client.on_message = self.mqtt_on_message
 
-        self.mqtt_client.connect(host='192.168.0.191', port=1883, keepalive=60)
+        self.mqtt_client.username_pw_set(credentials['username'], credentials['password'])
+        self.mqtt_client.will_set('master/core/available', 'offline', retain=True)
+        self.mqtt_client.connect(host=master_config['mqtt_server'], port=master_config['mqtt_port'], keepalive=60)
+
         self.last_time = {}
         self.lines_to_write = {}
         self.last_log_time = time.time()
         for i in range(12):
-            self.mqtt_client.subscribe(f'esp-module{i + 1}/uptime_slave')
-            self.mqtt_client.subscribe(f'esp-module{i + 1}/cell_voltage')  # TODO cell_voltages
-            self.mqtt_client.subscribe(f'esp-module{i + 1}/module_voltage')
-            self.mqtt_client.subscribe(f'esp-module{i + 1}/module_temps')
-            self.mqtt_client.subscribe(f'esp-module{i + 1}/chip_temp')
             self.last_time[i + 1] = 0
             self.lines_to_write[i + 1] = []
+
         self.mqtt_client.loop_start()
 
     def send_heartbeat(self):
@@ -31,52 +44,66 @@ class EasyBMSMaster:
         scheduler.enter(delay=1, priority=1, action=self.send_heartbeat)
 
     def mqtt_on_connect(self, client: mqtt.Client, userdata: Any, flags: Dict, rc: int):
-        pass
+        for i in range(12):
+            self.mqtt_client.subscribe(f'esp-module/{i + 1}/uptime')
+            for j in range(12):
+                self.mqtt_client.subscribe(f'esp-module/{i + 1}/cell/{j + 1}/voltage')
+                self.mqtt_client.subscribe(f'esp-module/{i + 1}/cell/{j + 1}/balance')
+            self.mqtt_client.subscribe(f'esp-module/{i + 1}/module_voltage')
+            self.mqtt_client.subscribe(f'esp-module/{i + 1}/module_temps')
+            self.mqtt_client.subscribe(f'esp-module/{i + 1}/chip_temp')
+        self.mqtt_client.publish('master/core/available', 'online', retain=True)
+
+    def write_logs(self):
+        self.last_log_time = time.time()
+        for i in self.lines_to_write:
+            with open(f'logs/esp-module{i}.log', 'a+') as file:
+                for line in self.lines_to_write[i]:
+                    print(line, file=file)
+                self.lines_to_write[i].clear()
+
+    @staticmethod
+    def topic_extract_number(topic: str) -> (int, str,):
+        number = topic[topic.find('/') + 1:]
+        sub_topic = number[number.find('/') + 1:]
+        number = int(number[:number.find('/')])
+        return number, sub_topic
 
     def mqtt_on_message(self, client: mqtt.Client, userdata: Any, msg: mqtt.MQTTMessage):
-        if time.time() - self.last_log_time >= 60:
-            self.last_log_time = time.time()
-            for i in self.lines_to_write:
-                with open(f'logs/esp-module{i}.log', 'a+') as file:
-                    for line in self.lines_to_write[i]:
-                        print(line, file=file)
-                    self.lines_to_write[i].clear()
-        if msg.topic.startswith('esp-module'):
-            esp_module = msg.topic[:msg.topic.find('/')]
-            esp_module_index = esp_module[10:]
-            if not esp_module_index.isnumeric():
-                self.lines_to_write[esp_module_index].append(f'{esp_module}: {esp_module_index} is not numeric!')
-                print(f'{esp_module}: {esp_module_index} is not numeric!')
-                return
-            esp_module_index = int(esp_module_index)
+        if time.time() - self.last_log_time >= 60:  # log time diffs
+            self.write_logs()
+
+        if msg.topic.startswith('esp-module/'):
+            esp_number, topic = self.topic_extract_number(msg.topic)
             if len(msg.payload) > 0:
-                battery_module = battery_system.battery_modules[esp_module_index - 1]  # TODO esp 0
-                if msg.topic.endswith('/uptime_slave'):
-                    # print(msg.topic, msg.payload)
-                    if not msg.payload.decode().isnumeric():
-                        self.lines_to_write[esp_module_index].append(f'{esp_module}: {msg.payload} is not numeric!')
-                        print(f'{esp_module}: {msg.payload} is not numeric!')
-                        return
-                    current_time = int(msg.payload)
+                payload = msg.payload.decode()
+                battery_module = battery_system.battery_modules[esp_number - 1]
+                if topic == 'uptime':
+                    current_time = int(payload)
                     battery_module.update_esp_uptime(current_time)
-                    if self.last_time[esp_module_index] != 0:
-                        diff = current_time - self.last_time[esp_module_index] - 1000
-                        self.mqtt_client.publish(topic=f'{esp_module}/timediff', payload=f'{diff}')
-                        self.lines_to_write[esp_module_index].append(f'{current_time},{diff}')
-                    self.last_time[esp_module_index] = current_time
-                    # print(msg.payload)
-                if msg.topic.endswith('/cell_voltage'):  # TODO cell_voltages
-                    cell_voltages = msg.payload.decode().split(',')
-                    cell_voltages.pop()
-                    for i, cell_voltage in enumerate(cell_voltages):
-                        battery_module.cells[i].update_voltage(float(cell_voltage))
-                if msg.topic.endswith('/module_voltage'):
-                    battery_module.update_module_voltage(float(msg.payload.decode()))
-                if msg.topic.endswith('/module_temps'):
-                    module_temps = msg.payload.decode().split(',')
+
+                    if self.last_time[esp_number] != 0:  # log time diffs
+                        diff = current_time - self.last_time[esp_number] - 1000
+                        self.mqtt_client.publish(topic=f'esp-module/{esp_number}/timediff', payload=f'{diff}')
+                        self.lines_to_write[esp_number].append(f'{current_time},{diff}')
+                    self.last_time[esp_number] = current_time
+
+                elif topic.startswith('cell/'):
+                    cell_number, sub_topic = self.topic_extract_number(topic)
+                    if sub_topic == 'voltage':
+                        battery_module.cells[cell_number - 1].update_voltage(float(payload))
+                    elif sub_topic == 'balance':
+                        if payload == 'balancing':
+                            battery_module.cells[cell_number - 1].balance_pin_state = True
+                        else:
+                            battery_module.cells[cell_number - 1].balance_pin_state = False
+                elif topic == 'module_voltage':
+                    battery_module.update_module_voltage(float(payload))
+                elif topic == 'module_temps':
+                    module_temps = payload.split(',')
                     battery_module.update_module_temps(module_temps[0], module_temps[1])
-                if msg.topic.endswith('/chip_temp'):
-                    battery_module.update_chip_temp(float(msg.payload.decode()))
+                elif topic == 'chip_temp':
+                    battery_module.update_chip_temp(float(payload))
 
 
 def balance_task():
@@ -85,11 +112,11 @@ def balance_task():
 
 
 if __name__ == '__main__':
-    number_of_battery_modules = 12
-    battery_system = BatterySystem(number_of_battery_modules)
+    config = get_config('config.yaml')
+    battery_system = BatterySystem(config['number_of_battery_modules'])
     battery_manager = BatteryManager(battery_system)
 
-    easy_bms_master = EasyBMSMaster()
+    easy_bms_master = EasyBMSMaster(config)
 
     scheduler = sched.scheduler()
     scheduler.enter(delay=0, priority=1, action=easy_bms_master.send_heartbeat)
