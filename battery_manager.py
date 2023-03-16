738d60dd167e55b33bd92719281c07640e076244
Jesse Kleemann <j.kleemann@tu-bs.de>
2023-03-16 13:25:11 +0100

    increase target range

--- battery_manager.py
+++ battery_manager.py
@@ -54,11 +54,11 @@ class BatteryManager:
         highest_voltage: float = cells.highest_voltage()
         if lowest_voltage <= BatteryCell.soc_to_voltage(0.2):
             self.slave_communicator.send_discharge_limit(False)
-        elif lowest_voltage >= BatteryCell.soc_to_voltage(0.22):
+        elif lowest_voltage >= BatteryCell.soc_to_voltage(0.23):
             self.slave_communicator.send_discharge_limit(True)
         if highest_voltage >= BatteryCell.soc_to_voltage(0.88):
             self.slave_communicator.send_charge_limit(False)
-        elif highest_voltage <= BatteryCell.soc_to_voltage(0.86):
+        elif highest_voltage <= BatteryCell.soc_to_voltage(0.85):
             self.slave_communicator.send_charge_limit(True)
 
     def trigger_safety_disconnect(self) -> None:
